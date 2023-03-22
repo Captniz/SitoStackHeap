3b941ac6e02b37e057a2469f858b30664415da2e
Simone-Tardiani <simone.tardiani@gmail.com>
2023-03-22 17:24:04 +0100

    Finished css and thos things

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,11 @@
-alert("bella");
+var activeTheme = "light";
+
+//TODO: RIFALLO PER LE LALTRE DUE PAGINE E ATTENTO A I LINK E CHE RESTI SEMPRE QUELLo
+function changeTheme() {
+  if (activeTheme == "light") {
+    activeTheme = "dark";
+  } else {
+    activeTheme = "light";
+  }
+  $("#csslink").prop("href", "./src/styles/" + activeTheme + ".css");
+}
